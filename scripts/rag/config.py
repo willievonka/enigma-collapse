68117ea3be536b922f715bb7b7e18dfc16bc3d07
yakovlev05 <alexey.yakovlev.05@mail.ru>
2026-02-27 23:54:24 +0500

    Add scripts for rag

---
+++ scripts/rag/config.py
@@ -0,0 +1,12 @@
+TXT_PATH = '../pardus.txt'
+
+OPENROUTER_API_KEY = REQUIRED
+OPENROUTER_MODEL = 'openai/text-embedding-3-small'
+OPENROUTER_API_URL = 'https://openrouter.ai/api/v1'
+
+QDRANT_URL = 'http://localhost:6333'
+QDRANT_COLLECTION = 'enigma_hack'
+
+HTTP_PROXY = 'http://127.0.0.1:2080'
+
+# Python 3.12
